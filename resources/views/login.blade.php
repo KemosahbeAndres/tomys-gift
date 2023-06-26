e521ee6bc3918d3637771dbe81c6bbe57a36d472
Andres Cubillos <andrestj1996@gmail.com>
2023-06-26 14:04:14 -0400

    Sitio base, estilos y scripts importados.

---
+++ resources/views/login.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="row">
+        <div class="col-md-6">
+            <h2>Col1</h2>
+        </div>
+        <div class="col-md-6">
+            <h2>Col2</h2>
+        </div>
+    </div>
+
+@endsection
